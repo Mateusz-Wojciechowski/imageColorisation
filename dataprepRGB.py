d8aa1f254a9fbe22aa7e41a99698270f27fa4067
Mateusz Wojciechowski <120648222+Mateusz-Wojciechowski@users.noreply.github.com>
2024-02-19 18:26:53 +0100

    Add files via upload

--- dataprepRGB.py
+++ dataprepRGB.py
@@ -15,21 +15,10 @@ torch.manual_seed(0)
 # dataset = GrayColorDataset(color_dir=r'Images/color', gray_dir=r'Images/gray', transform=transform)
 dataset = GrayColorDataset(color_dir=r'/content/my_data/landscape Images/color', gray_dir=r'/content/my_data/landscape Images/gray', transform=transform)
 
-train_size = int(0.3 * len(dataset))
+train_size = int(0.6 * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = data.random_split(dataset, [train_size, test_size])
 
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
-
-# for org, target in test_loader:
-#     image = org[0]
-#     target_im = target[0]
-#     break
-#
-# plt.imshow(image.permute(1, 2, 0), cmap='gray')
-# plt.show()
-#
-# plt.imshow(target_im.permute(1, 2, 0))
-# plt.show()
 
