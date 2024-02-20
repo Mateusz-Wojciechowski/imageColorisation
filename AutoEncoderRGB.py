b2710073de55199e2a17bce094a708da469eec44
Mateusz Wojciechowski <120648222+Mateusz-Wojciechowski@users.noreply.github.com>
2024-02-20 17:31:31 +0100

    Add files via upload

--- AutoEncoderRGB.py
+++ AutoEncoderRGB.py
@@ -1,34 +1,40 @@
-import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class AutoEncoderRGB(nn.Module):
     def __init__(self):
         super(AutoEncoderRGB, self).__init__()
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(16, 32, 3, stride=2, padding=1),
+            nn.Conv2d(3, 32, 3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 64, 3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.Conv2d(64, 128, 7),
-            nn.ReLU()
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, 3, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, 7),
+            nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1),
+            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.ConvTranspose2d(16, 3, 3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(32, 3, 3, stride=1, padding=1),
             nn.Sigmoid()
         )
 
-    def forward(self, data):
-        encoder_output = self.encoder(data)
-        decoder_output = self.decoder(encoder_output)
-        return decoder_output
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
