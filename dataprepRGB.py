4792e0fe741edf4ccc24e4f04b9f7c6ec7f371bd
Mateusz Wojciechowski <120648222+Mateusz-Wojciechowski@users.noreply.github.com>
2024-02-20 18:25:30 +0100

    Add files via upload

--- dataprepRGB.py
+++ dataprepRGB.py
@@ -15,7 +15,7 @@ torch.manual_seed(0)
 #dataset = GrayColorDataset(color_dir=r'Images/color', gray_dir=r'Images/gray', transform=transform)
 dataset = GrayColorDataset(color_dir=r'/content/my_data/landscape Images/color', gray_dir=r'/content/my_data/landscape Images/gray', transform=transform)
 
-train_size = int(0.8 * len(dataset))
+train_size = int(0.6 * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = data.random_split(dataset, [train_size, test_size])
 
@@ -25,8 +25,8 @@ test_indices = test_dataset.indices
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-torch.save(train_indices, 'train_indices75epochsNew.pt')
-torch.save(test_indices, 'test_indices75epochsNew.pt')
+# torch.save(train_indices, 'train_indices75epochsNew.pt')
+# torch.save(test_indices, 'test_indices75epochsNew.pt')
 
 
 
